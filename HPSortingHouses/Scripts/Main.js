d71f1a10b3927717e41ce36c8759ea1a4c66d5ba
mastermaarten <mastermaarten99@gmail.com>
2018-04-05 15:08:32 +0200

    here some good stff mate

--- HPSortingHouses/Scripts/Main.js
+++ HPSortingHouses/Scripts/Main.js
@@ -6,6 +6,7 @@ if (flag1 != null) {
 }
 */
 
+
 var flag1 = document.getElementById("flag1");
 var flag2 = document.getElementById("flag2");
 var bg = document.getElementById("background");
@@ -14,6 +15,7 @@ var cButtons = document.querySelectorAll(".cButton");
 flag1.style.top = "-100vh";
 flag1.style.top = 0;
 emblemBox = document.getElementById("emblem-box");
+
 
 function switchCarousel(x) {
 
@@ -44,3 +46,5 @@ function switchCarousel(x) {
     }
     x.classList.add("active");
 }
+
+
